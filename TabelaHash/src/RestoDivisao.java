841b90f3726a3af802721e22d2b6b36005d4b342
DaviMartins26 <davi.martins15@outlook.com>
2025-06-03 16:51:45 -0300

    Update RestoDivisao.java

--- TabelaHash/src/RestoDivisao.java
+++ TabelaHash/src/RestoDivisao.java
@@ -1,7 +1,16 @@
 public class RestoDivisao implements FuncaoHash {
+
     @Override
     public int hash(Registro registro, int tamanhoTabela) {
-        long codigo = Long.parseLong(registro.getCodigo());
-        return (int) (codigo % tamanhoTabela);
+        String codigo = registro.getCodigo();
+        long valor = 0;
+
+        for (int i = 0; i < codigo.length(); i++) {
+            char c = codigo.charAt(i);
+            int digito = c - '0'; // conversão manual
+            valor = valor * 10 + digito;
+        }
+
+        return (int) (valor % tamanhoTabela);
     }
 }
